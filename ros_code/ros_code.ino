e26afcde662a77bf7055cc1425df43f0705997d9
ihassnain <iitk.hassnain20@gmail.com>
2020-10-07 07:43:41 +0530

    Ros Code edited for Commands

--- ros_code/ros_code.ino
+++ ros_code/ros_code.ino
@@ -1,19 +1,21 @@
 /*
-   rosserial Subscriber 
+   rosserial Subscriber Example
+   Blinks an LED on callback
 */
 
 #include <ros.h>
 #include <std_msgs/Int32.h>
 
 ros::NodeHandle  nh;
-int buzzer = 11;
-int delay1 = 500;
-int delay2 = 1000;
-int delay3 = 1500;
-int delay4 = 2000;
-int tune_freq = 1000;
+int buzzer =8;
+int common_delay =500;
+int delay_stop =2000;
+int delay_reached =2000;
 
-void buzzup(int buzz_delay)
+int tune_freq = 1500;
+
+void buzzup(int buzz_delay, int numb)
+{for(int i=0;i<numb;i++)
 {
   tone(buzzer, tune_freq);
   delay(buzz_delay);
@@ -24,29 +26,33 @@ void buzzup(int buzz_delay)
   Serial.println(buzz_delay);
 
 }
+}
 
 
 
 void callback_buzzer( const std_msgs::Int32& msg) {
   
-
-  if (msg.data == 1)
+  if (msg.data == 403)
   {
 
-    buzzup(delay1);
+    buzzup(common_delay,2);
   }
-  else if (msg.data == 2)
+  else if (msg.data == 401)
   {
-    buzzup(delay2);
+    buzzup(common_delay ,3);
   }
-  else if (msg.data == 3)
+  else if (msg.data == 406)
   {
-    buzzup(delay3);
+    buzzup(common_delay, 4);
   }
 
-  else if (msg.data == 4)
+  else if (msg.data == 405)
   {
-    buzzup(delay4);
+    buzzup(delay_reached,2);
+  }
+  else if (msg.data == 400)
+  {
+    buzzup(delay_stop,1);
   }
   else if ( msg.data > 1000)
   {
